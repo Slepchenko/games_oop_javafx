fa92c79f40f74094c063bb0d1dc50c34c3a0b197
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-07 23:50:19 +0300

    2. Сокобан. Тестовое задание. [#409092]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,41 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        if (checkRow(board, findElement(board)[0]) || checkCell(board, findElement(board)[1])) {
+           return true;
+        }
+        return false;
     }
+
+    public static int[] findElement(int[][] board) {
+        int[] res = new int[2];
+        for (int row = 0; row < board.length; row++) {
+            for (int cell = 0; cell < board[row].length; cell++) {
+                if (board[row][cell] == 1) {
+                    res[0] = row;
+                    res[1] = cell;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static boolean checkRow(int[][] board, int rowIndex) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[rowIndex][row] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkCell(int[][] board, int cellIndex) {
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[cell][cellIndex] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -55,4 +55,64 @@ public class WinTest {
         };
         assertThat(Win.check(board), is(false));
     }
+
+    @Test
+    public void findElementWhenRow2Cell3() {
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+        };
+        assertThat(Win.findElement(board), is(new int[] {2, 3}));
+    }
+
+    @Test
+    public void findRowWhenRow2ThenFalse() {
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+        };
+        assertThat(Win.checkRow(board, 2), is(false));
+    }
+
+    @Test
+    public void findRowWhenRow2ThenTrue() {
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+        };
+        assertThat(Win.checkRow(board, 2), is(true));
+    }
+
+    @Test
+    public void findCellWhenCell3ThenFalse() {
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+        };
+        assertThat(Win.checkCell(board, 3), is(false));
+    }
+
+    @Test
+    public void findCellWhenCell3ThenTrue() {
+        int[][] board = {
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0},
+        };
+        assertThat(Win.checkCell(board, 3), is(true));
+    }
 }
